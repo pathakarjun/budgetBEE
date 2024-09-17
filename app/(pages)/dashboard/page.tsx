934f7decef4b543399e783e6bcdf75bd34895a4b
Arjun <pathakarjun2010@gmail.com>
2024-09-16 23:55:30 -0600

    Added logic for Total monthly income and expenses and reformatted recent transaction to table

--- app/(pages)/dashboard/page.tsx
+++ app/(pages)/dashboard/page.tsx
@@ -7,80 +7,183 @@ import { ScrollArea } from "@/components/ui/scroll-area";
 import { HandCoins, Wallet } from "lucide-react";
 import { signOut } from "next-auth/react";
 import { useEffect, useState } from "react";
+import { toast } from "sonner";
+import { Transaction } from "@/types/types";
 
 const page = () => {
-	const [userId, setUserId] = useState("");
+  const [userId, setUserId] = useState<string | null>(null);
+  const [totalIncome, setTotalIncome] = useState<number>(0);
+  const [totalExpense, setTotalExpense] = useState<number>(0);
+  const [transactionsData, setTransactionsData] = useState<Transaction[]>([]);
 
-	useEffect(() => {
-		const getSession = async () => {
-			const session = await fetch("/api/session");
-			const sessionData = await session.json();
-			if (sessionData) {
-				setUserId(sessionData.user.id);
-			}
-		};
-		getSession();
-	}, []);
+  const formatDate = (date: Date): string => {
+    return date.toISOString().split("T")[0];
+  };
 
-	return (
-		<div className="w-full flex h-svh max-h-svh">
-			<div className="h-full flex-[0.25]"></div>
+  const formatCurrency = (amount: number): string => {
+    return new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
+    })
+      .format(amount)
+      .replace("$", "$ ");
+  };
 
-			<ScrollArea className="h-full flex-1 bg-gray-100 w-full">
-				<div className="flex-1 space-y-4 p-4">
-					<h2 className="text-2xl font-medium tracking-tight">Overview</h2>
-					<div className="grid grid-cols-3 grid-rows-4 gap-4">
-						<div className="col-span-2">
-							<div className="flex flex-row">
-								<div className="flex gap-8 flex-row pt-5">
-									<Card className="bg-inherit border-none shadow-none">
-										<CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2 pl-0">
-											<CardTitle className="text-sm font-normal text-gray-700">
-												Income
-											</CardTitle>
-											<Wallet color="rgb(52 211 153)" size={14} />
-										</CardHeader>
-										<CardContent className="pl-0">
-											<div className="text-xl font-medium">$4,355.89</div>
-										</CardContent>
-									</Card>
-									<div className="inline-block h-[75px] min-h-[1em] w-0.5 bg-gray-200 self-center"></div>
-									<Card className="bg-inherit border-none shadow-none">
-										<CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-											<CardTitle className="text-sm font-normal text-gray-700">
-												Expense
-											</CardTitle>
-											<HandCoins color="rgb(251 113 133)" size={14} />
-										</CardHeader>
-										<CardContent>
-											<div className="text-xl font-medium">$2,145.46</div>
-										</CardContent>
-									</Card>
-								</div>
-							</div>
-						</div>
-						<div className="row-span-4 pt-5">
-							<RecordTransactionForm userId={userId} />
-						</div>
-						<Card className="row-span-3 col-span-2">
-							<CardHeader>
-								<CardTitle className="text-lg">Overview</CardTitle>
-							</CardHeader>
-							<CardContent className="pl-2"></CardContent>
-						</Card>
-					</div>
-					<Card>
-						<CardHeader>
-							<CardTitle className="text-lg">Recent Transactions</CardTitle>
-						</CardHeader>
-						<CardContent>
-							<RecentTransactions />
-						</CardContent>
-					</Card>
-				</div>
-			</ScrollArea>
-		</div>
-	);
+  const getMonthlyTransaction = async () => {
+    if (!userId) return;
+
+    const today = new Date();
+    const firstDay = new Date(today.getFullYear(), today.getMonth(), 1);
+    const lastDay = new Date(today.getFullYear(), today.getMonth() + 1, 0);
+
+    try {
+      const params = new URLSearchParams({
+        userId: userId,
+        firstDate: formatDate(firstDay),
+        lastDate: formatDate(lastDay),
+      });
+      const response = await fetch(`/api/transactions?${params.toString()}`, {
+        method: "GET",
+      });
+
+      if (!response.ok) {
+        toast.error("Network response was not ok");
+      }
+
+      const responseData: Transaction[] = await response.json();
+
+      if (responseData) {
+        let totalIncome = 0;
+        let totalExpense = 0;
+
+        responseData.forEach((item) => {
+          if (item.transaction_type === "Income") {
+            totalIncome += item.amount;
+          } else if (item.transaction_type === "Expense") {
+            totalExpense += item.amount;
+          }
+        });
+
+        setTotalIncome(totalIncome);
+        setTotalExpense(totalExpense);
+      }
+    } catch (error) {
+      console.error("Error fetching transactions:", error);
+    }
+  };
+
+  const fetchRecentTransactions = async () => {
+    if (!userId) return;
+    try {
+      const params = new URLSearchParams({ userId });
+      const response = await fetch(`/api/transactions?${params.toString()}`, {
+        method: "GET",
+      });
+
+      if (!response.ok) {
+        toast.error("Network response was not ok");
+      }
+      const responseData: Transaction[] = await response.json();
+
+      if (responseData) {
+        setTransactionsData(responseData);
+      }
+    } catch (error) {
+      console.error("Error fetching recent transactions:", error);
+    }
+  };
+
+  useEffect(() => {
+    const getSession = async () => {
+      const session = await fetch("/api/session");
+      const sessionData = await session.json();
+      if (sessionData) {
+        setUserId(sessionData.user.id);
+      }
+    };
+    getSession();
+  }, []);
+
+  useEffect(() => {
+    getMonthlyTransaction();
+    fetchRecentTransactions();
+  }, [userId]);
+
+  const handleFormSubmit = async () => {
+    await getMonthlyTransaction();
+    await fetchRecentTransactions();
+  };
+
+  return (
+    <div className="w-full flex h-svh max-h-svh">
+      <div className="h-full flex-[0.25]"></div>
+
+      <ScrollArea className="h-full flex-1 bg-gray-100 w-full">
+        <div className="flex-1 space-y-4 p-10">
+          <h2 className="text-2xl font-medium tracking-tight">
+            Monthly Overview
+          </h2>
+          <div className="grid grid-cols-3 grid-rows-4 gap-4">
+            <div className="col-span-2">
+              <div className="flex flex-row">
+                <div className="flex gap-8 flex-row pt-5">
+                  <Card className="bg-inherit border-none shadow-none">
+                    <CardHeader className="flex flex-row items-center justify-between space-y-0 gap-10 pb-2 pl-0">
+                      <CardTitle className="text-sm font-normal text-gray-700">
+                        Income
+                      </CardTitle>
+                      <Wallet color="rgb(52 211 153)" size={14} />
+                    </CardHeader>
+                    <CardContent className="pl-0">
+                      <div className="text-xl font-medium">
+                        {formatCurrency(totalIncome)}
+                      </div>
+                    </CardContent>
+                  </Card>
+                  <div className="inline-block h-[75px] min-h-[1em] w-0.5 bg-gray-200 self-center"></div>
+                  <Card className="bg-inherit border-none shadow-none">
+                    <CardHeader className="flex flex-row items-center justify-between space-y-0 gap-10 pb-2">
+                      <CardTitle className="text-sm font-normal text-gray-700">
+                        Expense
+                      </CardTitle>
+                      <HandCoins color="rgb(251 113 133)" size={14} />
+                    </CardHeader>
+                    <CardContent>
+                      <div className="text-xl font-medium">
+                        {formatCurrency(totalExpense)}
+                      </div>
+                    </CardContent>
+                  </Card>
+                </div>
+              </div>
+            </div>
+            <div className="row-span-4 pt-5">
+              <RecordTransactionForm
+                userId={userId || ""}
+                onSuccess={handleFormSubmit}
+              />
+            </div>
+            <Card className="row-span-3 col-span-2">
+              <CardHeader>
+                <CardTitle className="text-lg">Expenses</CardTitle>
+              </CardHeader>
+              <CardContent className="pl-2"></CardContent>
+            </Card>
+          </div>
+          <Card>
+            <CardHeader>
+              <CardTitle className="text-lg">Recent Transactions</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <RecentTransactions transactionsData={transactionsData} />
+            </CardContent>
+          </Card>
+        </div>
+      </ScrollArea>
+    </div>
+  );
 };
 
 export default page;
--- app/api/transactions/route.ts
+++ app/api/transactions/route.ts
@@ -4,94 +4,103 @@ import * as z from "zod";
 
 //Define a schema for input validation
 const transactionsSchema = z.object({
-	description: z.string().min(1, "Description is required").max(200),
-	type: z.string().min(1, "Transaction type is required").max(50),
-	classification: z
-		.string()
-		.min(1, "Transaction classification is required")
-		.max(50),
-	transactionDate: z.string().datetime(),
-	amount: z.coerce.number().positive(),
-	userId: z.string(),
+  description: z.string().min(1, "Description is required").max(200),
+  type: z.string().min(1, "Transaction type is required").max(50),
+  classification: z
+    .string()
+    .min(1, "Transaction classification is required")
+    .max(50),
+  transactionDate: z.string().datetime(),
+  amount: z.coerce.number().positive(),
+  userId: z.string(),
 });
 
 export async function GET(req: any) {
-	try {
-		const url = new URL(req.url);
-		const searchParam = new URLSearchParams(url.searchParams);
-		const findTransactions = await db.transactions.findMany({
-			where: {
-				user_id: searchParam.get("userId") ?? "",
-				transaction_type: searchParam.get("transactionType") ?? undefined,
-				transaction_subtype: searchParam.get("transactionSubtype") ?? undefined,
-			},
-			select: {
-				description: true,
-				transaction_type: true,
-				transaction_subtype: true,
-				transaction_date: true,
-				amount: true,
-			},
-		});
+  try {
+    const url = new URL(req.url);
+    const searchParam = new URLSearchParams(url.searchParams);
+    const firstDateParam = searchParam.get("firstDate");
+    const lastDateParam = searchParam.get("lastDate");
+    const findTransactions = await db.transactions.findMany({
+      where: {
+        user_id: searchParam.get("userId") ?? "",
+        transaction_type: searchParam.get("transactionType") ?? undefined,
+        transaction_subtype: searchParam.get("transactionSubtype") ?? undefined,
+        transaction_date: {
+          gte: firstDateParam ? new Date(firstDateParam) : undefined,
+          lte: lastDateParam ? new Date(lastDateParam) : undefined,
+        },
+      },
+      select: {
+        description: true,
+        transaction_type: true,
+        transaction_subtype: true,
+        transaction_date: true,
+        amount: true,
+      },
+      orderBy: {
+        create_dt: "desc",
+      },
+    });
 
-		return NextResponse.json(findTransactions);
-	} catch (error) {
-		console.log(error);
-		return NextResponse.json(
-			{ message: "Something went wrong!" },
-			{ status: 500 }
-		);
-	}
+    return NextResponse.json(findTransactions);
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      { message: "Something went wrong!" },
+      { status: 500 }
+    );
+  }
 }
 
 export async function POST(req: Request) {
-	try {
-		const body = await req.json();
-		const {
-			description,
-			type,
-			classification,
-			transactionDate,
-			amount,
-			userId,
-		} = transactionsSchema.parse(body);
+  try {
+    const body = await req.json();
+    const {
+      description,
+      type,
+      classification,
+      transactionDate,
+      amount,
+      userId,
+    } = transactionsSchema.parse(body);
 
-		const classificationIdentifier =
-			await db.transaction_classifications.findUnique({
-				where: {
-					tnClassification_identifier: {
-						user_id: userId,
-						transaction_type: type,
-						transaction_subtype: classification,
-					},
-				},
-			});
+    const classificationIdentifier =
+      await db.transaction_classifications.findUnique({
+        where: {
+          tnClassification_identifier: {
+            user_id: userId,
+            transaction_type: type,
+            transaction_subtype: classification,
+          },
+        },
+      });
 
-		if (!classificationIdentifier) {
-			throw new Error("Transaction classification not found");
-		}
+    if (!classificationIdentifier) {
+      throw new Error("Transaction classification not found");
+    }
 
-		const newtransaction = await db.transactions.create({
-			data: {
-				description: description,
-				transaction_type: type,
-				transaction_subtype: classification,
-				transaction_date: transactionDate,
-				amount: amount,
-				user_id: userId,
-				transaction_classificationId: classificationIdentifier.id,
-			},
-		});
+    const newtransaction = await db.transactions.create({
+      data: {
+        description: description,
+        transaction_type: type,
+        transaction_subtype: classification,
+        transaction_date: transactionDate,
+        amount: amount,
+        user_id: userId,
+        transaction_classificationId: classificationIdentifier.id,
+      },
+    });
 
-		return NextResponse.json(
-			{ user: newtransaction, message: "Transaction registered successfully" },
-			{ status: 201 }
-		);
-	} catch (error) {
-		console.log(error);
-		return NextResponse.json(
-			{ message: "Something went wrong!" },
-			{ status: 500 }
-		);
-	}
+    return NextResponse.json(
+      { user: newtransaction, message: "Transaction registered successfully" },
+      { status: 201 }
+    );
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      { message: "Something went wrong!" },
+      { status: 500 }
+    );
+  }
 }
