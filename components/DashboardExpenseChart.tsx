7fd325a25b8f6105e2378b9933a6b5b35cd2b171
Pathak <arjpathak@deloitte.com>
2024-09-18 17:01:00 -0600

    Added month picker to show the monthly overview in sync

--- components/DashboardExpenseChart.tsx
+++ components/DashboardExpenseChart.tsx
@@ -2,206 +2,206 @@ import { Transaction } from "@/types/types";
 import { ChartContainer, ChartTooltip, ChartTooltipContent } from "./ui/chart";
 import { Pie, PieChart } from "recharts";
 import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
+	Card,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
 } from "./ui/card";
 import { format } from "date-fns";
 
 type DashboardExpenseChartProps = {
-  transactionsData: Transaction[];
+	transactionsData: Transaction[];
+	date: Date;
 };
 
 interface ChartDatum {
-  category: string;
-  amount: number;
-  fill: string;
+	category: string;
+	amount: number;
+	fill: string;
 }
 
 interface ChartConfig {
-  amount: { label: string };
-  [key: string]: { label: string; color: string } | { label: string };
+	amount: { label: string };
+	[key: string]: { label: string; color: string } | { label: string };
 }
 
 const DashboardExpenseChart = ({
-  transactionsData,
+	transactionsData,
+	date,
 }: DashboardExpenseChartProps) => {
-  const today = new Date();
+	function generateUniqueColor(index: number): string {
+		const hue = (index * 137.508) % 360;
+		return `hsl(${hue}, 60%, 70%)`; // Return a pastel-like color
+	}
 
-  function generateUniqueColor(index: number): string {
-    const hue = (index * 137.508) % 360;
-    return `hsl(${hue}, 60%, 70%)`; // Return a pastel-like color
-  }
+	function generateExpenseChartData(transactionsData: Transaction[]): {
+		expenseChartData: ChartDatum[];
+		expenseChartConfig: ChartConfig;
+	} {
+		const groupedExpenses = transactionsData.reduce(
+			(acc: { [key: string]: number }, transaction: Transaction) => {
+				if (transaction.transaction_type === "Expense") {
+					if (!acc[transaction.transaction_subtype]) {
+						acc[transaction.transaction_subtype] = 0;
+					}
+					acc[transaction.transaction_subtype] += transaction.amount;
+				}
+				return acc;
+			},
+			{}
+		);
 
-  function generateExpenseChartData(transactionsData: Transaction[]): {
-    expenseChartData: ChartDatum[];
-    expenseChartConfig: ChartConfig;
-  } {
-    const groupedExpenses = transactionsData.reduce(
-      (acc: { [key: string]: number }, transaction: Transaction) => {
-        if (transaction.transaction_type === "Expense") {
-          if (!acc[transaction.transaction_subtype]) {
-            acc[transaction.transaction_subtype] = 0;
-          }
-          acc[transaction.transaction_subtype] += transaction.amount;
-        }
-        return acc;
-      },
-      {}
-    );
+		const expenseChartConfig: ChartConfig = {
+			amount: { label: "Amount" },
+		};
 
-    const expenseChartConfig: ChartConfig = {
-      amount: { label: "Amount" },
-    };
+		const expenseChartData: ChartDatum[] = Object.entries(groupedExpenses).map(
+			([subtype, total], index) => {
+				const color = generateUniqueColor(index);
 
-    const expenseChartData: ChartDatum[] = Object.entries(groupedExpenses).map(
-      ([subtype, total], index) => {
-        const color = generateUniqueColor(index);
+				expenseChartConfig[subtype] = {
+					label: subtype,
+					color: color,
+				};
 
-        expenseChartConfig[subtype] = {
-          label: subtype,
-          color: color,
-        };
+				const expenseChartDatum: ChartDatum = {
+					category: subtype,
+					amount: total,
+					fill: color,
+				};
 
-        const expenseChartDatum: ChartDatum = {
-          category: subtype,
-          amount: total,
-          fill: color,
-        };
+				return expenseChartDatum;
+			}
+		);
 
-        return expenseChartDatum;
-      }
-    );
+		return { expenseChartData, expenseChartConfig };
+	}
 
-    return { expenseChartData, expenseChartConfig };
-  }
+	function generateIncomeChartData(transactionsData: Transaction[]): {
+		incomeChartData: ChartDatum[];
+		incomeChartConfig: ChartConfig;
+	} {
+		const groupedIncomes = transactionsData.reduce(
+			(acc: { [key: string]: number }, transaction: Transaction) => {
+				if (transaction.transaction_type === "Income") {
+					if (!acc[transaction.transaction_subtype]) {
+						acc[transaction.transaction_subtype] = 0;
+					}
+					acc[transaction.transaction_subtype] += transaction.amount;
+				}
+				return acc;
+			},
+			{}
+		);
 
-  function generateIncomeChartData(transactionsData: Transaction[]): {
-    incomeChartData: ChartDatum[];
-    incomeChartConfig: ChartConfig;
-  } {
-    const groupedIncomes = transactionsData.reduce(
-      (acc: { [key: string]: number }, transaction: Transaction) => {
-        if (transaction.transaction_type === "Income") {
-          if (!acc[transaction.transaction_subtype]) {
-            acc[transaction.transaction_subtype] = 0;
-          }
-          acc[transaction.transaction_subtype] += transaction.amount;
-        }
-        return acc;
-      },
-      {}
-    );
+		const incomeChartConfig: ChartConfig = {
+			amount: { label: "Amount" },
+		};
 
-    const incomeChartConfig: ChartConfig = {
-      amount: { label: "Amount" },
-    };
+		const incomeChartData: ChartDatum[] = Object.entries(groupedIncomes).map(
+			([subtype, total], index) => {
+				const color = generateUniqueColor(index);
 
-    const incomeChartData: ChartDatum[] = Object.entries(groupedIncomes).map(
-      ([subtype, total], index) => {
-        const color = generateUniqueColor(index);
+				incomeChartConfig[subtype] = {
+					label: subtype,
+					color: color,
+				};
 
-        incomeChartConfig[subtype] = {
-          label: subtype,
-          color: color,
-        };
+				const incomeChartDatum: ChartDatum = {
+					category: subtype,
+					amount: total,
+					fill: color,
+				};
 
-        const incomeChartDatum: ChartDatum = {
-          category: subtype,
-          amount: total,
-          fill: color,
-        };
+				return incomeChartDatum;
+			}
+		);
 
-        return incomeChartDatum;
-      }
-    );
+		return { incomeChartData, incomeChartConfig };
+	}
 
-    return { incomeChartData, incomeChartConfig };
-  }
+	const { expenseChartData, expenseChartConfig } =
+		generateExpenseChartData(transactionsData);
 
-  const { expenseChartData, expenseChartConfig } =
-    generateExpenseChartData(transactionsData);
+	const { incomeChartData, incomeChartConfig } =
+		generateIncomeChartData(transactionsData);
 
-  const { incomeChartData, incomeChartConfig } =
-    generateIncomeChartData(transactionsData);
+	console.log(incomeChartData);
 
-  console.log(incomeChartData);
-
-  return (
-    <div className="flex flex-row gap-4 h-full w-full">
-      <Card className="flex flex-col w-1/2">
-        <CardHeader className="pb-0">
-          <CardTitle className="text-xl">Income</CardTitle>
-        </CardHeader>
-        <CardContent className="flex-1 pb-0">
-          <ChartContainer config={incomeChartConfig} className="w-full h-full">
-            {incomeChartData.length > 0 ? (
-              <PieChart className="w-full h-full">
-                <ChartTooltip
-                  cursor={false}
-                  content={<ChartTooltipContent hideLabel />}
-                />
-                <Pie
-                  data={incomeChartData}
-                  dataKey="amount"
-                  nameKey="category"
-                  innerRadius={60}
-                />
-              </PieChart>
-            ) : (
-              <div className="flex items-center w-full h-full justify-center">
-                <div className="text-sm leading-none text-muted-foreground text-center">
-                  No income added for {format(today, "MMM, yyyy")}
-                </div>
-              </div>
-            )}
-          </ChartContainer>
-        </CardContent>
-        {incomeChartData.length > 0 && (
-          <CardFooter className="text-sm leading-none text-muted-foreground justify-center text-center">
-            Showing income for {format(today, "MMM, yyyy")}
-          </CardFooter>
-        )}
-      </Card>
-      <Card className="flex flex-col w-1/2">
-        <CardHeader className="pb-0">
-          <CardTitle className="text-xl">Expenses</CardTitle>
-        </CardHeader>
-        <CardContent className="flex-1 pb-0">
-          <ChartContainer config={expenseChartConfig} className="w-full h-full">
-            {expenseChartData.length > 0 ? (
-              <PieChart>
-                <ChartTooltip
-                  cursor={false}
-                  content={<ChartTooltipContent hideLabel />}
-                />
-                <Pie
-                  data={expenseChartData}
-                  dataKey="amount"
-                  nameKey="category"
-                  innerRadius={60}
-                />
-              </PieChart>
-            ) : (
-              <div className="flex items-center w-full h-full justify-center">
-                <div className="text-sm leading-none text-muted-foreground text-center">
-                  No expenses added for {format(today, "MMM, yyyy")}
-                </div>
-              </div>
-            )}
-          </ChartContainer>
-        </CardContent>
-        {expenseChartData.length > 0 && (
-          <CardFooter className="text-sm leading-none text-muted-foreground justify-center text-center">
-            Showing expenses for {format(today, "MMM, yyyy")}
-          </CardFooter>
-        )}
-      </Card>
-    </div>
-  );
+	return (
+		<div className="flex flex-row gap-4 h-full w-full">
+			<Card className="flex flex-col w-1/2">
+				<CardHeader className="pb-0">
+					<CardTitle className="text-xl">Income</CardTitle>
+				</CardHeader>
+				<CardContent className="flex-1 pb-0">
+					<ChartContainer config={incomeChartConfig} className="w-full h-full">
+						{incomeChartData.length > 0 ? (
+							<PieChart className="w-full h-full">
+								<ChartTooltip
+									cursor={false}
+									content={<ChartTooltipContent hideLabel />}
+								/>
+								<Pie
+									data={incomeChartData}
+									dataKey="amount"
+									nameKey="category"
+									innerRadius={60}
+								/>
+							</PieChart>
+						) : (
+							<div className="flex items-center w-full h-full justify-center">
+								<div className="text-sm leading-none text-muted-foreground text-center">
+									No income added for {format(date, "MMM, yyyy")}
+								</div>
+							</div>
+						)}
+					</ChartContainer>
+				</CardContent>
+				{incomeChartData.length > 0 && (
+					<CardFooter className="text-sm leading-none text-muted-foreground justify-center text-center">
+						Showing income for {format(date, "MMM, yyyy")}
+					</CardFooter>
+				)}
+			</Card>
+			<Card className="flex flex-col w-1/2">
+				<CardHeader className="pb-0">
+					<CardTitle className="text-xl">Expenses</CardTitle>
+				</CardHeader>
+				<CardContent className="flex-1 pb-0">
+					<ChartContainer config={expenseChartConfig} className="w-full h-full">
+						{expenseChartData.length > 0 ? (
+							<PieChart>
+								<ChartTooltip
+									cursor={false}
+									content={<ChartTooltipContent hideLabel />}
+								/>
+								<Pie
+									data={expenseChartData}
+									dataKey="amount"
+									nameKey="category"
+									innerRadius={60}
+								/>
+							</PieChart>
+						) : (
+							<div className="flex items-center w-full h-full justify-center">
+								<div className="text-sm leading-none text-muted-foreground text-center">
+									No expenses added for {format(date, "MMM, yyyy")}
+								</div>
+							</div>
+						)}
+					</ChartContainer>
+				</CardContent>
+				{expenseChartData.length > 0 && (
+					<CardFooter className="text-sm leading-none text-muted-foreground justify-center text-center">
+						Showing expenses for {format(date, "MMM, yyyy")}
+					</CardFooter>
+				)}
+			</Card>
+		</div>
+	);
 };
 
 export default DashboardExpenseChart;
