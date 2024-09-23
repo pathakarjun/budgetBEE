5edd18a6f2f9c359072caddc09183044b348def9
Pathak <arjpathak@deloitte.com>
2024-09-23 16:57:17 -0600

    Added avatar

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
 import { getServerSession } from "next-auth";
-import Sidebar from "@/components/Sidebar";
 
 const inter = Inter({ subsets: ["latin"] });
 
