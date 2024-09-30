55e5e3bd4c07838410d69b0957e4e01ed7a65331
Pathak <arjpathak@deloitte.com>
2024-09-30 09:48:28 -0600

    fix for not fetching last day of the month transaction

--- app/(pages)/dashboard/page.tsx
+++ app/(pages)/dashboard/page.tsx
@@ -54,7 +54,15 @@ const page = () => {
 		if (!userId) return;
 
 		const firstDay = new Date(date.getFullYear(), date.getMonth(), 1);
-		const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0);
+		const lastDay = new Date(
+			date.getFullYear(),
+			date.getMonth() + 1,
+			0,
+			23,
+			59,
+			59,
+			999
+		);
 
 		try {
 			const params = new URLSearchParams({
